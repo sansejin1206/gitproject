2310e5309cd5a48c89f03b3cb4a98f6b77d5343e
sansejin <r777998@163.com>
2020-09-15 19:52:12 +0800

    这是提交的第一个项目

---
+++ src/main/java/com/qupojie/project/Test01.java
@@ -0,0 +1,16 @@
+package com.qupojie.project;
+
+public class Test01 {
+   public static void main(String[] args) {
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+      System.out.println("这是一个git的测试项目");
+   }
+}
+
