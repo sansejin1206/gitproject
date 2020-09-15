4be27a23faab73560b09f8b474dd8b690eb8be7b
sansejin <r777998@163.com>
2020-09-15 19:54:14 +0800

    这是修改了之后的

--- src/main/java/com/qupojie/project/Test01.java
+++ src/main/java/com/qupojie/project/Test01.java
@@ -11,6 +11,10 @@ public class Test01 {
       System.out.println("这是一个git的测试项目");
       System.out.println("这是一个git的测试项目");
       System.out.println("这是一个git的测试项目");
+   if("a"=="a"){
+      System.out.println("添加的内容");
    }
+   }
+
 }
 
